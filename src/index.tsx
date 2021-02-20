9262a7e400057c36b5398759b71991e3d85a176a
dongsubak <likeap@gmail.com>
2021-02-20 11:32:39 +0900

    react apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { ApolloProvider } from "react-apollo";
 import ReactDOM from 'react-dom';
-// import './index.css';
+import client from './apollo'
 import App from './App';
+
+// import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}><App /></ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 //ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 
 // If you want to start measuring performance in your app, pass a function
