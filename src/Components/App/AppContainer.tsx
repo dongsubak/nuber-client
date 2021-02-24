db0a6e6dabb1d81179894c3df7fef79a0050a5cc
dongsubak <likeap@gmail.com>
2021-02-24 12:02:29 +0900

    global-styles

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import GlobalStyle from "../../global-styles";
 import { theme } from "../../theme"
 import { ThemeProvider } from "../../typed-components";
 import AppPresenter from "./AppPresenter"
 import { IS_LOGGED_IN } from "./AppQueries";
 
+
+//const GlobalStyle = createGlobalStyle`${reset}`
+
 const AppContainer: any = ({ data }) => (
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
   </ThemeProvider>
   //<div>{JSON.stringify(data)</div>
