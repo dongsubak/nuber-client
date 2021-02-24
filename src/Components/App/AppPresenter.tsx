d716043ea8cfc655b84393100c067f1b6cab61d3
dongsubak <likeap@gmail.com>
2021-02-24 12:10:34 +0900

    create, import routes indexes

--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,5 +1,18 @@
 import PropTypes from "prop-types";
 import React from "react";
+
+import AddPlace from "../../Routes/AddPlace";
+import EditAccount from "../../Routes/EditAccount";
+import Home from "../../Routes/Home";
+import OutHome from "../../Routes/OutHome";
+import PhoneLogin from "../../Routes/PhoneLogin";
+import Places from "../../Routes/Places";
+import Ride from "../../Routes/Ride";
+import Settings from "../../Routes/Settings";
+import VerifyPhone from "../../Routes/VerifyPhone";
+
+
+
 //import styled from "../../typed-components";
 
 //const Thing = styled.div`
---
+++ src/Routes/EditAccount/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const index = () => <span></span>;
+
+export default index;
